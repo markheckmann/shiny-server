a5316efea6baa112463feac49c4fe0665e9434b1
Mark Heckmann <heckmann.mark@gmail.com>
2019-11-17 15:18:34 +0100

    add openrepgrid.ic folder

---
+++ openrepgrid.ic/app.R
@@ -0,0 +1,26 @@
+# latest release version of shiny app
+# installed in custom R lib
+
+library(shiny)
+options(shiny.sanitize.errors = FALSE)
+package <- "OpenRepGrid.ic"
+rlib <- "/usr/share/R/rlib"
+  
+# add custom library with production version on top of libpath
+.libPaths(new = c(rlib, .libPaths()) )
+library(gridsampler)
+
+inst <- rownames(installed.packages())
+if (package %in% inst) {
+  appDir <- system.file("shiny", package = package)
+  setwd(appDir)
+  shiny::shinyAppDir(".")
+  
+} else {
+  server <- function(input, output){}
+    ui <- shiny::fluidPage(
+      shiny::titlePanel(paste0("Package '", package, "' was not found."))
+  )
+  shiny::shinyApp(ui = ui, server = server)
+}
+ 
